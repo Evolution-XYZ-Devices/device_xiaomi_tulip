1d13925a8bf88c7f8bed063987bcfe5229a336c6
XsaviXander <22182013+XsaviXander@users.noreply.github.com>
2020-02-12 01:51:30 +0000

    tulip: Enable freeform window management
    Change-Id: I2de7aab9d42c33989b53052266350a45982273db
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -201,6 +201,10 @@ PRODUCT_PACKAGES += \
     libqti_vndfwk_detect \
     libqti_vndfwk_detect.vendor
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
