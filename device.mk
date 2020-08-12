f5b91199e722d2aadcfcf8a536b65eabfe55666d
Lucchetto <lucchetto.tie@live.com>
2020-08-12 14:14:36 +0200

    twolip: Create dummy libqti-perfd-client
    * Proprietary perfd blobs can finally be nuked without breaking goodix
    * We could even map the functions to use libperfmgr powerhints in the future
    
    Change-Id: I124652f3041761966a3e3bd97c757fecc39cc5fb
    Signed-off-by: Ratoriku <a1063021545@gmail.com>

--- device.mk
+++ device.mk
@@ -325,7 +325,8 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-libperfmgr \
-    android.hardware.power.stats@1.0-service.mock
+    android.hardware.power.stats@1.0-service.mock \
+    libqti-perfd-client
 
 # Protobuf
 PRODUCT_PACKAGES += \
