c2509c492035b413b66187617ce0ab83cfb55dfb
Lucchetto <lucchetto.tie@live.com>
2020-08-12 14:14:36 +0200

    tulip: Create dummy libqti-perfd-client
    * Proprietary perfd blobs can finally be nuked without breaking goodix
    * We could even map the functions to use libperfmgr powerhints in the future
    
    Change-Id: I124652f3041761966a3e3bd97c757fecc39cc5fb
    Signed-off-by: Ratoriku <a1063021545@gmail.com>

--- device.mk
+++ device.mk
@@ -305,7 +305,8 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.tulip-libperfmgr \
-    android.hardware.power.stats@1.0-service.mock
+    android.hardware.power.stats@1.0-service.mock \
+    libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
