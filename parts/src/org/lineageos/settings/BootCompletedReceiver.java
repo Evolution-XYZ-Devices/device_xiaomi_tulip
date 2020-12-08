abb218aa25e876d507ccaba88b23b8383642146e
daniml3 <danimoral1001@gmail.com>
2020-12-08 19:32:53 +0100

    tulip: parts: KCAL fixes and improvements
    * Initialize the services before writing KCAL settings
    * Properly handle situations when KCAL is not supported by the kernel
    
    Signed-off-by: daniml3 <danimoral1001@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -34,7 +34,9 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
 
-        KcalUtils.writeCurrentSettings(sharedPrefs);
+        if (KcalUtils.isKcalSupported())
+            KcalUtils.writeCurrentSettings(sharedPrefs);
+
         DozeUtils.checkDozeService(context);
     }
 }
