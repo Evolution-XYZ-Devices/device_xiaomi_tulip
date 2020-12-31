113e83ba29d4ea434ae5666673e111751324d330
Chenyang Zhong <zhongcy95@gmail.com>
2020-12-30 21:49:49 -0500

    tulip: Hardcode LD and AR for custom clang
    The LD and AR were hard-coded to CLANG_PREBUILTS, which is
    clang-r450784d as of writing. If a device builds kernel with a
    newer version of LLVM/Clang, the hard-coded LLD may encounter
    unsupported features such as
    
    ld.lld: error: built-in.o(init/main.o): Opaque pointers are only
    supported in -opaque-pointers mode (Producer: 'LLVM15.0.0git'
    Reader: 'LLVM 14.0.6git')
    
    Therefore, only hard-code LD and AR when KERNEL_TOOLCHAIN is not set.
    
    Change-Id: Ic707afc3ddf8101a734ccbd9f7a924952a20f6e3
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,6 +53,8 @@ TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm660
 TARGET_KERNEL_CONFIG := tulip_defconfig
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
+TARGET_KERNEL_ADDITIONAL_FLAGS += LD=$(shell pwd)/prebuilts/clang/host/linux-x86/clang-prelude/bin/ld.lld
+TARGET_KERNEL_ADDITIONAL_FLAGS += AR=$(shell pwd)/prebuilts/clang/host/linux-x86/clang-prelude/bin/llvm-ar
 TARGET_KERNEL_CLANG_VERSION := prelude
 KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-prelude/bin
 
