d9b63af64a196317dcb3da6f2f4929d22e407e15
LuK1337 <priv.luk@gmail.com>
2021-06-10 09:24:34 +0200

    tulip: Build android.hardware.thermal@2.0-service.qti
    * Fixes:
    E ThermalHalWrapper: Thermal HAL 2.0 service not connected.
    E ThermalHalWrapper: Thermal HAL 1.1 service not connected.
    E ThermalHalWrapper: Thermal HAL 1.0 service not connected.
    W ThermalManagerService: No Thermal HAL service on this device
    
    Change-Id: I950ab50ded7b3c1164ff88ab18d7f1e3365a6086
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- device.mk
+++ device.mk
@@ -423,6 +423,10 @@ PRODUCT_PACKAGES += \
     libcamera_sdm660_shim \
     libpiex_shim
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.qti
+
 # Tetheroffload
 PRODUCT_PACKAGES += \
     ipacm \
