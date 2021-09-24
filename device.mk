5d5c345449f8f116d7605832e7a3a57fb145f0e1
Ratoriku <a1063021545@gmail.com>
2021-09-24 21:13:12 +0800

    twolip: Switch to AIDL Light HAL
    Signed-off-by: Ratoriku <a1063021545@gmail.com>
    Change-Id: I2618bcb81902688b9b9b975f612c653707787202

--- device.mk
+++ device.mk
@@ -289,7 +289,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.twolip
+    android.hardware.lights-service.twolip
 
 # LiveDisplay native
 PRODUCT_PACKAGES += \
