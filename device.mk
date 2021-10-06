da28368c81d35be3853268478b1f8c81336e0425
Marijn Suijten <marijns95@gmail.com>
2021-10-06 22:44:02 +0200

    tulip: Provide android.system.net.netd@1.1 on vendor for netmgrd
    Following the previous commit, starting with Android S most HAL
    libraries are not included in VNDK anymore - they need to be explicitly
    pulled in to `/vendor` for odm blobs to be allowed to access them:
    
        linker  : CANNOT LINK EXECUTABLE "/odm/bin/netmgrd": library "android.system.net.netd@1.1.so" not found: needed by main executable
    
    Signed-off-by: Marijn Suijten <marijn.suijten@somainline.org>

--- device.mk
+++ device.mk
@@ -277,6 +277,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+# Network
+PRODUCT_PACKAGES += \
+    android.system.net.netd@1.1.vendor
+
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
