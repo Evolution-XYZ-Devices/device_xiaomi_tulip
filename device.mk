48b02aac4e50af6738b1370384e84c44a443cfe7
SakilMondal <mondalsakil@gmail.com>
2021-10-06 20:14:13 +0000

    tulip: Build rest of vendor variants of hidl HALs
    Change-Id: I478e164b30e323a359d999c0fc8e734dbecb9be3

--- device.mk
+++ device.mk
@@ -117,6 +117,7 @@ PRODUCT_PACKAGES += \
 
 # Biometrics
 PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1.vendor \
     android.hardware.biometrics.fingerprint@2.1-service.tulip
 
 PRODUCT_PACKAGES += \
@@ -207,6 +208,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gnss@2.0-impl-qti \
     android.hardware.gnss@2.0-service-qti \
+    android.hardware.gnss@2.0.vendor \
     libbatching \
     libgeofencing \
     libgnss \
@@ -275,6 +277,9 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.tulip
 
 # Media
+PRODUCT_PACKAGES += \
+    android.hardware.media.c2@1.1.vendor
+
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(DEVICE_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
@@ -287,6 +292,7 @@ PRODUCT_COPY_FILES += \
 
 # Network
 PRODUCT_PACKAGES += \
+    android.hardware.neuralnetworks@1.3.vendor \
     android.system.net.netd@1.1.vendor
 
 # OMX
