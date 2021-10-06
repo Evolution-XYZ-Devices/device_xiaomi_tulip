3fc991a789e6db743b54d5cc00e579ca79b1d466
Marijn Suijten <marijn.suijten@somainline.org>
2021-10-06 22:14:46 +0200

    tulip: Provide latest radio@1.6 and radio.config@1.3 HAL libs
    On Lena we're observing much more recent libraries being used - this is
    not a problem on Android R where these libraries are available in VNDK,
    but AOSP stripped them all in Android S [1] where these dependency
    issues surface.
    
    [1]: https://cs.android.com/android/_/android/platform/hardware/interfaces/+/d610435ac4bb051761b0f016aa6cdf2e884c55b5
    
    Signed-off-by: Marijn Suijten <marijn.suijten@somainline.org>

--- device.mk
+++ device.mk
@@ -359,8 +359,8 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.4.vendor \
-    android.hardware.radio.config@1.2.vendor \
+    android.hardware.radio@1.6.vendor \
+    android.hardware.radio.config@1.3.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
     android.hardware.secure_element@1.1 \
     android.hardware.secure_element@1.2 \
