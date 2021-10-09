5f3d5d2277c92f702b0aa1df6155969954d6d3ad
arturo51236 <rtsanonynousperson@gmail.com>
2021-10-09 10:30:49 +0200

    tulip: Evox-Ify
    Signed-off-by: Arturo <rtsanonynousperson@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_tulip.mk
+    $(LOCAL_DIR)/evolution_tulip.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_tulip-user \
-    aosp_tulip-userdebug \
-    aosp_tulip-eng
+    evolution_tulip-user \
+    evolution_tulip-userdebug \
+    evolution_tulip-eng
