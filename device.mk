1774dd0b8b1b1c6f6646df53de14fc6d6402d8e9
arturo51236 <rtsanonynousperson@gmail.com>
2022-02-27 03:24:59 +0100

    tulip: remove_packages: Nuke all the unwanted packages
    Signed-off-by: Arturo <rtsanonynousperson@gmail.com>

--- device.mk
+++ device.mk
@@ -292,7 +292,7 @@ PRODUCT_PACKAGES += \
 
 # Remove unwanted packages
 PRODUCT_PACKAGES += \
-    RemovePackages
+    RemovePkgs
 
 # Power
 PRODUCT_PACKAGES += \
--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -1,17 +1,17 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := RemovePackages
+LOCAL_MODULE := RemovePkgs
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AppDirectedSMSService
-LOCAL_OVERRIDES_PACKAGES += CarrierSetup ConnMO DCMO
-LOCAL_OVERRIDES_PACKAGES += DMService
-LOCAL_OVERRIDES_PACKAGES += MyVerizonServices
-LOCAL_OVERRIDES_PACKAGES += OBDM_Permissions Showcase
-LOCAL_OVERRIDES_PACKAGES += SprintDM SprintHM USCCDM
-LOCAL_OVERRIDES_PACKAGES += VZWAPNLib VzwOmaTrigger obdm_stub
+LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AppDirectedSMSService BetterBug Camera2 CarrierMetrics CarrierSetup CarrierWifi ConnMO DCMO
+LOCAL_OVERRIDES_PACKAGES += DMService DeviceIntelligenceNetworkPrebuilt DevicePolicyPrebuilt DiagnosticsToolPrebuilt Drive FilesPrebuilt GCS
+LOCAL_OVERRIDES_PACKAGES += HelpRtcPrebuilt LocationHistoryPrebuilt MaestroPrebuilt Maps MarkupGoogle MicropaperPrebuilt MyVerizonServices
+LOCAL_OVERRIDES_PACKAGES += NgaResources OdadPrebuilt OemDmTrigger Photos PixelLiveWallpaperPrebuilt PrebuiltGmail RecorderPrebuilt
+LOCAL_OVERRIDES_PACKAGES += SCONE SafetyHubPrebuilt ScribePrebuilt SecurityHubPrebuilt Showcase Snap SoundAmplifierPrebuilt SprintDM
+LOCAL_OVERRIDES_PACKAGES += SprintHM TurboAdapter TurboPrebuilt Tycho USCCDM VZWAPNLib VzwOmaTrigger WellbeingPrebuilt WfcActivation
+LOCAL_OVERRIDES_PACKAGES += arcore obdm_stub
 LOCAL_UNINSTALLABLE_MODULE := true
-LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_CERTIFICATE := platform
 LOCAL_SRC_FILES := /dev/null
 include $(BUILD_PREBUILT)
