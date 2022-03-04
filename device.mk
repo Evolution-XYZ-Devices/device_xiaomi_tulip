1cb6251aaf133f846ca65c52b6aeefb799fd730c
Etienne Ruffieux <eruffieux@google.com>
2022-03-04 13:31:43 +0000

    tulip: Removed audio A2dp from device.mk
    Bug: 220306859
    Test: atest
    Tag: #feature
    Change-Id: I1af8461f8ad82afbb21b43dd6968e824ea89f5d6

--- device.mk
+++ device.mk
@@ -78,7 +78,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl:32 \
     android.hardware.audio.effect@5.0-impl:32 \
     android.hardware.audio.service \
-    audio.a2dp.default \
     audio.primary.sdm660 \
     audio.r_submix.default \
     audio.usb.default \
