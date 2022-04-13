30ad946a50dd0ac87920ea77d4b4a4fee7193109
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-04-14 01:48:37 +0200

    twolip: Switch to common IFAA
    * From hardware/xiaomi
    
    Change-Id: I71bfdacc4626d75d56aa12ec384f0f116d6d6ff2

--- device.mk
+++ device.mk
@@ -118,11 +118,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.twolip
 
+# IFAA
 PRODUCT_PACKAGES += \
-    org.ifaa.android.manager
-
-PRODUCT_BOOT_JARS += \
-    org.ifaa.android.manager
+    IfaaService
 
 # Bluetooth
 PRODUCT_PACKAGES += \
