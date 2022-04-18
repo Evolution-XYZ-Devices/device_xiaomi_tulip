6fc06f046fb379e89a7c94d4e033a814a0141d3c
chandu078 <chandudyavanapelli03@gmail.com>
2022-04-19 00:03:13 +0200

    tulip: Drop mock hidl power stats
    - E android.hardware.power.stats@1.0-service-mock Failed to getEnergyData
    
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>
    Signed-off-by: drkphnx <dark.phnx12@gmail.com>

--- device.mk
+++ device.mk
@@ -320,7 +320,6 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-libperfmgr \
-    android.hardware.power.stats@1.0-service.mock \
     libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
