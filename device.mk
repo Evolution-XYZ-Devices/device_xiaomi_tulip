9ab2498977dc233dc6e6774f5ef70057b873225e
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-12 01:34:35 +0200

    tulip: rootdir: Move to Android.bp
    * Also move ueventd.rc to /vendor/etc/ueventd.rc
    
    Change-Id: I0b540cfd6756790443d7622f74d44d4c82dc41e4

--- device.mk
+++ device.mk
@@ -249,13 +249,15 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     fstab.zram \
     init.class_main.sh \
-    init.msm.usb.configfs.rc \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
+    init.qcom.usb.sh \
+    
+PRODUCT_PACKAGES += \
+    init.msm.usb.configfs.rc \
     init.qcom.rc \
     init.qcom.sh \
     init.qcom.usb.rc \
-    init.qcom.usb.sh \
     init.recovery.qcom.rc \
     init.target.rc \
     ueventd.qcom.rc \
