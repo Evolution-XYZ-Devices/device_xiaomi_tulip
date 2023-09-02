f10e26fa122b058cde1102b1474f66ae9464900e
GhostMaster69-dev <rathore6375@gmail.com>
2023-09-02 12:45:33 +0000

    tulip: rro: Cleanup DeviceConfigOverlay
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- device.mk
+++ device.mk
@@ -19,7 +19,6 @@ PRODUCT_PACKAGES += \
     ApertureOverlay \
     CarrierConfigOverlay \
     CellBroadcastReceiverOverlay \
-    DeviceConfigOverlay \
     FrameworksResOverlay \
     SettingsResOverlay \
     SystemUIResOverlay \
