c1aea8fecb93cdc1738d564cb3a7149975b19fe6
GhostMaster69-dev <rathore6375@gmail.com>
2023-10-14 20:46:26 +0530

    Revert "tulip: Remove vendor RenderScript implementation"
    This reverts commit 99d26764338d79079552e16d61ed20c8773814a7.
    
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- device.mk
+++ device.mk
@@ -183,6 +183,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
+    android.hardware.renderscript@1.0-impl \
     android.frameworks.displayservice@1.0 \
     vendor.display.config@1.1
 
@@ -386,6 +387,10 @@ PRODUCT_PACKAGES += \
 # QNS
 PRODUCT_PACKAGES += \
     libstdc++.vendor
+
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
 
 # RIL
 PRODUCT_PACKAGES += \
