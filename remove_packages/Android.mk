4589469c17c9cb70fab505b4ea2de327a92bd967
GhostMaster69-dev <rathore6375@gmail.com>
2023-10-27 00:24:05 +0530

    tulip: remove_packages: Provide Google package by default
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePkgs
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AndroidAutoStubPrebuilt CalculatorGooglePrebuilt CalendarGooglePrebuilt Chrome-Stub GoogleFeedback GooglePrintRecommendationService GoogleTTS NfcNci NoCutoutOverlay Photos PrebuiltDeskClockGoogle ScribePrebuilt Symphonica TagGoogle talkback Velvet
+LOCAL_OVERRIDES_PACKAGES := AndroidAutoStubPrebuilt CalculatorGooglePrebuilt CalendarGooglePrebuilt Chrome-Stub GoogleFeedback GooglePrintRecommendationService GoogleTTS NfcNci NoCutoutOverlay Photos PrebuiltDeskClockGoogle ScribePrebuilt Symphonica TagGoogle talkback
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
