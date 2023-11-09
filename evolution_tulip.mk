941e80fa467c83daeaffbff2b4c8543dc1eb1b58
Juhyung Park <qkrwngud825@gmail.com>
2023-11-09 12:35:10 +0530

    tulip: Stop shipping 32-bit Zygote
    Pixel 7 onwards also do the same, meaning that the user inconvenience is
    none if not minimal.
    
    This saves non-negligible amount of RAM.
    
    Change-Id: Ide8671817d15e79ab61a1665cf3f60e599ff3981

--- evolution_tulip.mk
+++ evolution_tulip.mk
@@ -5,7 +5,7 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Evolution X stuff
