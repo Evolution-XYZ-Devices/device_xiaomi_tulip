730d99c5c65d74f169b9d8eabf2e667488bc2615
Le Hong Duc <hongducthbk123@gmail.com>
2024-03-17 23:07:09 +0700

    Revert "tulip: overlay: Use MlkitBarcodeUI as default QR scanner"
    This reverts commit 2a9fae59f56e9214ab00b8855fc4324ade7831bc.

--- device.mk
+++ device.mk
@@ -375,12 +375,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
-# Prebuilt packages
-PRODUCT_PACKAGES += \
-    MlkitBarcodeUI \
-    VisionBarcode \
-    QRCameraOverlay
-
 # Protobuf
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-vendorcompat \
