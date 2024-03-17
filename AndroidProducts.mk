a5cca57d923a0b273461b7a54c00e17f512eb76c
Le Hong Duc <hongducthbk123@gmail.com>
2024-03-17 23:30:29 +0700

    tulip: Evo-ify
    Signed-off-by: Le Hong Duc <hongducthbk123@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_tulip.mk
+    $(LOCAL_DIR)/evolution_tulip.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_tulip-user \
-    derp_tulip-userdebug \
-    derp_tulip-eng
+    evolution_tulip-user \
+    evolution_tulip-userdebug \
+    evolution_tulip-eng
